a4f1bca4f546612c6c7c9a09b90bd9cd64c1ee7a
Timothy Weber <tweber@mit.edu>
2025-03-07 13:32:10 -0500

    set up package, add alt reading method

---
+++ dirigo_ni_frame_grabber/__init__.py
@@ -0,0 +1,3 @@
+from dirigo_ni_frame_grabber.dirigo_ni_frame_grabber import NIFrameGrabber
+
+__all__ = ['NIFrameGrabber']
--- dirigo_ni_frame_grabber/dirigo_ni_frame_grabber.py
+++ dirigo_ni_frame_grabber/dirigo_ni_frame_grabber.py
@@ -7,11 +7,22 @@ class NIFrameGrabber(FrameGrabber):
     def __init__(self, device_name: str, **kwargs):
         self._board = IMAQBoard(device_name)
 
-    def write_serial(self, message):
+    def serial_write(self, message):
         self._board.session_serial_write(message)
     
-    def read_serial(self):
+    def serial_read(self):
+        """
+        Reads up to the specified termination character (e.g. \r).
+        """
         message = self._board.session_serial_read()
+        return message
+    
+    def serial_read_nbytes(self, nbytes):
+        """
+        Alternative read method that reads individual bytes and does not stop at 
+        terminator characters.
+        """
+        message = self._board.session_serial_read_bytes(nbytes)
         return message
 
 
