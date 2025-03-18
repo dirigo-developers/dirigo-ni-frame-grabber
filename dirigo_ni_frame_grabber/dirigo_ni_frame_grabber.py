d4f48d006d6e5ba186db824d98a9fdc09701f7e4
Timothy Weber <tweber@mit.edu>
2025-03-18 10:39:16 -0400

    added buffers setup

--- dirigo_ni_frame_grabber/dirigo_ni_frame_grabber.py
+++ dirigo_ni_frame_grabber/dirigo_ni_frame_grabber.py
@@ -1,4 +1,9 @@
+import ctypes
+from ctypes import POINTER, c_int8
+
 from imaqbindings import Board as IMAQBoard
+from imaqbindings import Buffer as IMAQBuffer
+from imaqbindings import enumerations as Im
 
 from dirigo.hw_interfaces.camera import FrameGrabber
 
@@ -24,7 +29,73 @@ class NIFrameGrabber(FrameGrabber):
         """
         message = self._board.session_serial_read_bytes(nbytes)
         return message
+    
+    @property
+    def total_pixels_width(self):
+        return self._board.get_attribute(
+            Im.SessionInformation.IMG_ATTR_ACQWINDOW_WIDTH
+        )
 
+    @property
+    def roi_height(self):
+        return self._board.get_attribute(Im.Image.IMG_ATTR_ROI_HEIGHT)
+
+    @property
+    def roi_width(self):
+        return self._board.get_attribute(Im.Image.IMG_ATTR_ROI_WIDTH)
+
+    @property
+    def bytes_per_pixel(self):
+        return self._board.get_attribute(Im.Image.IMG_ATTR_BYTESPERPIXEL)
+    
+    def prepare_buffers(self, nbuffers: int):
+        """
+        Creates a contiguous "superbuffer" with shape (nbuffers, height, width).
+        Then sets up each buffer-list element to point into that block at the 
+        correct offset.
+        """
+        # create the buffer list
+        self._board.create_buf_list(nbuffers)
+
+        # Create the super-buffer. This is a single NI-allocated block shaped as (n_buffers, roi_height, roi_width).
+        self.super_buffer = IMAQBuffer(
+            board=self._board,
+            shape=(nbuffers, self.roi_height, self.roi_width),
+            bytes_per_pixel=self.bytes_per_pixel
+        )
+
+        base_address = self.super_buffer._adr # start of the superbuffer
+        # for each sub-buffer ...
+        for i in range(nbuffers):
+            # compute sub-buffer address, provide pointer
+            offset = i * self.bytes_per_buffer
+            buffer_ptr = ctypes.cast(base_address + offset, POINTER(c_int8))
+            self._board.set_buffer_element_address(i, buffer_ptr)
+
+            # set sub-buffer command
+            if i == (nbuffers - 1):
+                cmd = Im.BufferCommand.IMG_CMD_STOP
+            else:
+                cmd = Im.BufferCommand.IMG_CMD_NEXT
+            self._board.set_buffer_element_command(i, cmd)
+
+            # set buffer size
+            self._board.set_buffer_element_size(i, self.bytes_per_buffer) 
+
+        self._board.session_configure()
+
+    def start(self):
+        self._board.session_acquire(async_flag=True)
+
+    def stop(self):
+        self._board.session_abort()
+
+    def get_next_completed_superbuffer(self):
+        return self.super_buffer.buffer
+
+    @property
+    def buffers_acquired(self):
+        return self._board.get_attribute(Im.StatusInformation.IMG_ATTR_FRAME_COUNT)
 
 # For testing
 if __name__ == "__main__":
